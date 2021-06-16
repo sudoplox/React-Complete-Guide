125587e16a5517b49efc07741ed61f0c899b818c
Sudhanshu Mohan <sudhanshu17110@iiitd.ac.in>
2021-06-17 02:51:21 +0530

    custom components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    //JSX CODE SYNTAX
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
